2f197de48dd1bd4ecb6824bfbe7b880273d2f5b1
ahmedzoha029 <57677449+ahmedzoha029@users.noreply.github.com>
2022-02-14 22:53:19 -0700

    Added banking app setup and removed story board from app

---
+++ Banking/Banking/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Banking
+//
+//  Created by Zoha Ahmed on 2022-02-14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?)-> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+
+        return true
+    }
+
+
+}
+
